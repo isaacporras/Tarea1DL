18fb722136967ba28005a7006d2845efb21f57b4
Isaac Porras <imanoisaac@gmail.com>
2019-09-10 22:22:57 -0600

    Con funcionalidad de grafica

--- main.py
+++ main.py
@@ -49,7 +49,13 @@ class Demo1:
 class Demo2:
     def __init__(self, master, numero):
         print(numero)
-        print(binarizar(numero))
+
+        binario = binarizar(numero)
+
+        while len(binario)!= 12:
+            binario = "0" + binario
+
+        print(binario)
         self.master = master
         self.master.geometry("1500x500+10+100")
 
@@ -59,8 +65,18 @@ class Demo2:
         diferencial = 105
         x = 120
 
-        canvas.create_line(15, 250, 1400, 250)
-        canvas.create_line(15, 30, 15, 450)
+        canvas.create_line(15, 30, 15, 450)  # linea horizontal x
+        canvas.create_line(15, 250, 1400, 250) # linea horizontal y
+
+        alto_bit_x1 = 15
+        alto_bit_x2 = 120
+        alto_bit_y1 = 130
+        alto_bit_y2 = 130
+
+        bajo_bit_x1 = 15
+        bajo_bit_x2 = 120
+        bajo_bit_y1 = 370
+        bajo_bit_y2 = 370
 
         canvas.create_line(x, 100, x, 400, dash=(4, 2))
         canvas.create_line(x + 1*diferencial, 100, x + 1*diferencial, 400, dash=(4, 2))
@@ -74,7 +90,39 @@ class Demo2:
         canvas.create_line(x + 9 * diferencial, 100, x + 9 * diferencial, 400, dash=(4, 2))
         canvas.create_line(x + 10 * diferencial, 100, x + 10 * diferencial, 400, dash=(4, 2))
         canvas.create_line(x + 11* diferencial, 100, x + 11 * diferencial, 400, dash=(4, 2))
-        canvas.create_line(x + 12 * diferencial, 100, x + 12 * diferencial, 400, dash=(4, 2))
+
+
+
+        estado_actual = "alto"
+        bit_actual = 0
+        for num in binario:
+            if estado_actual == "alto":
+                if num == "1":
+                    estado_actual = "bajo"
+                    print("Cambia de estado a bajo")
+                elif num == "0":
+                    estado_actual = "alto"
+                    print("El numero se queda en alto")
+
+            elif estado_actual == "bajo":
+                if num == "1":
+                    estado_actual = "alto"
+                    print("Cambia de estado a alto")
+                elif num == "0":
+                    estado_actual = "bajo"
+                    print("El numero se queda en bajo")
+
+            if estado_actual == "alto":
+                canvas.create_line(alto_bit_x1 + bit_actual*diferencial, alto_bit_y1, alto_bit_x2+ bit_actual*diferencial, alto_bit_y2, fill="red")
+                bit_actual = bit_actual + 1
+            if estado_actual == "bajo":
+                canvas.create_line(bajo_bit_x1 + bit_actual*diferencial, bajo_bit_y1, bajo_bit_x2 + bit_actual*diferencial, bajo_bit_y2, fill="red")
+                bit_actual = bit_actual + 1
+
+            print(num)
+
+
+
 
         canvas.pack(fill=BOTH, expand=1)
 
